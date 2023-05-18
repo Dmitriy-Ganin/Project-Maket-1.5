84a807d5845834b1936b7cc58e0fc1912646ea67
Dmitriy Ganin <mrdmitriyganin@gmail.com>
2023-05-18 20:27:56 +0300

    1

--- js/swiper.js
+++ js/swiper.js
@@ -3,36 +3,35 @@ const slider = document.querySelector('.swiper-container');
 let mySwiper;
 
 function mobileSlider() {
-    if (window.innerWidth < 767 && slider.dataset.mobile == 'false') {
-        mySwiper = new Swiper(slider, {
-          direction: 'horizontal',
-            width: 320,
-            slidesPerView: 1.3,
-            spaceBetween: 16,
-            pagination: {
-                el: '.pagination',
-                clickable: true,
-            },
-            mousewheel: {
-              sensitivity: 1,
-              eventsTarget: '.swiper-container'
-            },
-        });
+  if (window.matchMedia('(max-width: 767px)') && slider.dataset.mobile == 'false') {
+    mySwiper = new Swiper(slider, {
+      direction: 'horizontal',
+      width: 320,
+      slidesPerView: 1.3,
+      spaceBetween: 16,
+      pagination: {
+        el: '.pagination',
+        clickable: true,
+      },
+      mousewheel: {
+        sensitivity: 1,
+        eventsTarget: '.swiper-container'
+      },
+    });
 
-        slider.dataset.mobile = 'true';
+    slider.dataset.mobile = 'true';
+  }
+
+  else {
+    slider.dataset.mobile = 'false';
+    if (mySwiper) {
+      mySwiper.destroy();
     }
-
-    if (window.innerWidth > 767) {
-        slider.dataset.mobile = 'false';
-
-        if (slider.classList.contains('swiper-initialized')) {
-            mySwiper.destroy();
-        }
-    }
+  }
 }
 
 mobileSlider();
 
-window.addEventListener('resize', () => {
-    mobileSlider();
+window.addEventListener('change', () => {
+  mobileSlider();
 })
